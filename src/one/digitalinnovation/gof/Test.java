1d3e1c42ea46f75811fa6883c42f90a3e1a063c3
Marcio Farias Pinto <marcio_fpinto@hotmail.com>
2022-08-01 00:49:58 -0300

    Final do desafio sobre padrões de projeto
    Projeto criado com padrões em java:  Utilizando singleton, strategy e facade.

---
+++ src/one/digitalinnovation/gof/Test.java
@@ -0,0 +1,50 @@
+package one.digitalinnovation.gof;
+
+import one.digitalinnovation.gof.facade.Facade;
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
+
+public class Test {
+    public static void main(String[] args){
+        // Testes Singleton:
+
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+
+        // Test Strategy
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensive = new ComportamentoDefensivo();
+        Comportamento aggressive = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensive);
+        robo.mover();
+        robo.setComportamento(aggressive);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+        // Test Facade
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Pedro", "12457823");
+    }
+}
